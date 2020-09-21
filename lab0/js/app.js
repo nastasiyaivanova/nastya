d554a78e720a2cc6e99ca1995da1ce8ed27e3a30
nastasiyaivanova <71224798+nastasiyaivanova@users.noreply.github.com>
2020-09-21 19:11:59 +0300

    Add files via upload

--- lab0/js/app.js
+++ lab0/js/app.js
@@ -1,19 +1,24 @@
-function clock(){
+function clock() {
 
   var date = new Date();
   var hours = date.getHours();
   var minutes = date.getMinutes();
   var seconds = date.getSeconds();
 
-  if(hours < 10)
+  if (hours < 10) {
     hours = "0" + hours;
-  if(minutes < 10)
+  }
+
+  if (minutes < 10) {
     minutes = "0" + minutes;
-  if(seconds < 10)
+  }
+
+  if (seconds < 10) {
     seconds = "0" + seconds;
+  }
 
   var str = hours + ":" + minutes + ":" + seconds;
 
-    document.getElementById("clock").innerHTML = str;
+  document.getElementById("clock").innerHTML = str;
 };
 setInterval("clock()", 1000);
